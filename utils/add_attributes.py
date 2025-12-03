d9a35bf1c42238c57cb1f8dc1a27d55264fa331e
Alberto Ferraz <aferraz@guselperroguia.com>
2025-12-03 11:53:45 +0100

    added getKeyFrameCameraInfo to utils

--- utils/add_attributes.py
+++ utils/add_attributes.py
@@ -6,18 +6,25 @@ def add_attributes_to_geo_meshes(asset_name, asset_info):
     meshes_in_asset = mc.listRelatives(f"{asset_name}|geo", ad=True, type='mesh', f=True)
 
     for mesh in meshes_in_asset:
+        print(f"---------------- Adding attributes to '{mesh}'")
         # First Clean possible old publish attributes
         try:
             mc.deleteAttr(f"{mesh}.GUS_relatedShader")
         except:
             pass
+
         add_attributes(mesh, asset_info)
 
 
 def add_attributes(mesh, asset_info):
 
     # Get specific info for that mesh
-    shading_engine = mc.listConnections(mesh, source=False, destination=True,type='shadingEngine')[0]
+    shading_engine = mc.listConnections(mesh, source=False, destination=True, type='shadingEngine')
+    if not shading_engine:
+        print("ERROR: No Shader Engine on this mesh, please assign one firs!")
+    else:
+        shading_engine = shading_engine[0]
+
     asset_info.update({"GUS_shading_grp": shading_engine})
 
     # Create Attributes
@@ -25,16 +32,16 @@ def add_attributes(mesh, asset_info):
 
         if not mc.attributeQuery(key, node=mesh, exists=True):
 
-            if type(asset_info[key]) == str:
+            if isinstance(asset_info[key], str):
                 mc.addAttr(mesh, longName=key, dataType='string')
-            elif type(asset_info[key])==int:
+            elif isinstance(asset_info[key], int):
                 mc.addAttr(mesh, longName=key, at='long')
-            elif type(asset_info[key])==float:
+            elif isinstance(asset_info[key], float):
                 mc.addAttr(mesh, longName=key, at='double')
-            elif type(asset_info[key])==bool:
+            elif isinstance(asset_info[key], bool):
                 mc.addAttr(mesh, longName=key, at='bool')
 
-        if type(asset_info[key]) == str:
+        if isinstance(asset_info[key], str):
 
             mc.setAttr(f"{mesh}.{key}", lock=False)
             mc.setAttr(f"{mesh}.{key}", asset_info[key], type='string')
@@ -47,7 +54,6 @@ def add_attributes(mesh, asset_info):
             mc.setAttr(f"{mesh}.{key}", lock=True)
 
 """
-
 #USAGE EXAMPLE
 
 asset_info = {'asset_name':'bancoCalle',
