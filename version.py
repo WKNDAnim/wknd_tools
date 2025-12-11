93cbb5daa5b810d95dc5b850f5a1040469bbefcb
Alberto Ferraz <aferraz@guselperroguia.com>
2025-12-11 18:45:39 +0100

    testing

--- version.py
+++ version.py
@@ -18,11 +18,11 @@ Release Types:
 import os
 import subprocess
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __version_info__ = {
     "major": 1,
     "minor": 0,
-    "patch": 3,
+    "patch": 4,
     "release": "stable",  # stable, beta, alpha
     "build": None
 }
