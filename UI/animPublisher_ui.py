164c7cc9e41eecf3f5af3bd170569b2840bcc916
Alberto Ferraz <aferraz@guselperroguia.com>
2026-04-09 16:35:43 +0200

    ## [1.1.10] - 2026-04-09 -- 2

--- UI/animPublisher_ui.py
+++ UI/animPublisher_ui.py
@@ -290,8 +290,8 @@ class AnimPubUI(MayaQWidgetDockableMixin, qt.QWidget):
                     mergeNamespacesOnClash=False,
                     namespace=f"exported_{p}",
                 )
-                warn = "The exported abc files have been referenced in the scene, please check all is fine before continue :)"
-                mc.confirmDialog(title='Finishing...', message=warn, button=['Okay'])
+            warn = "The exported abc files have been referenced in the scene, please check all is fine before continue :)"
+            mc.confirmDialog(title='Finishing...', message=warn, button=['Okay'])
 
             self.publish_btn.setText(msg)
 
