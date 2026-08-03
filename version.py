7899f121d69bb716fab3c5185f8d5b7394d7f07a
Alberto Ferraz <aferraz@guselperroguia.com>
2026-08-03 09:47:32 +0200

    ## [1.2.18] - 2026-08-03

--- version.py
+++ version.py
@@ -18,11 +18,11 @@ Release Types:
 import os
 import subprocess
 
-__version__ = "1.2.17"
+__version__ = "1.2.18"
 __version_info__ = {
     "major": 1,
     "minor": 2,
-    "patch": 17,
+    "patch": 18,
     "release": "stable",  # stable, beta, alpha
     "build": None
 }
